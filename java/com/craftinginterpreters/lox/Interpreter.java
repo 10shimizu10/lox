ac0cdeba7f7c5fa6402528b467cf8856ec238721
aoshimiz <aoshimiz@c2r2s3.42tokyo.jp>
2024-04-25 20:10:59 +0900

    finished c.8

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,17 +1,74 @@
 package com.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object>{
+import java.util.List;
 
-    void interpret(Expr expression){
+class Interpreter implements Expr.Visitor<Object>, Stmt.VAR<void>{
+    private Environment environment = new Enviornment();
+    void interpret(List<Stmt> statements){
         try{
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
-        } catch(RuntimeError){
-            Lox.RuntimeError(error);
+            for(Stmt statement : statements){
+                execute(statement);
+            }
+        } catch(RuntimeError error){
+            Lox.runtimeError(error);
         }
     }
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt){
+        stmt.accept(this);
+    }
+
+    void executeBlock(List<Stmt> statements, Enviornment environment){
+        Enviornment previous = this.environment;
+        try{
+            this.Enviornment = environment;
+
+            for(Stmt statement : statements){
+                execute(statement);
+            }
+        }finally{
+            this.environment = previous;
+        }
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt){
+        executeBlock(stmt.statements, new Enviornment(environment));
+        return null
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt){
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt){
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt){
+        Object value = null;
+        if(stmt.initializer != null){
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr){
+        Object value = evaluate(expr.value);
+        environment.assing(expr.name, value);
+        return value;
     }
 
     @Override
@@ -85,6 +142,11 @@ class Interpreter implements Expr.Visitor<Object>{
                 return -(double)right;
         }
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr){
+        return environment.get(expr.name);
     }
 
     private boolean isTruthy(Object object){
