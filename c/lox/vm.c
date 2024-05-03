e88eb6cac7992c4cd4cd4c3e1b525a10b8588a65
aoshimiz <aoshimiz@c2r2s1.42tokyo.jp>
2024-05-03 20:38:12 +0900

    fixed error

--- c/lox/vm.c
+++ c/lox/vm.c
@@ -114,7 +114,7 @@ static InterpretResult run(){
             case OP_GREATER:    BINARY_OP(BOOL_VAL, >); break;
             case OP_LESS:       BINARY_OP(BOOL_VAL, <); break;
             case OP_ADD: {
-                if(IS_STRING(peek(0)) && IS_NUMBER(peek(1))){
+                if(IS_STRING(peek(0)) && IS_STRING(peek(1))){
                     concatenate();
                 }else if(IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))){
                     double b = AS_NUMBER(pop());
