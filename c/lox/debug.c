58371291c7d15d35cff20150a20630187efbcc43
aoshimiz <aoshimiz@c2r2s1.42tokyo.jp>
2024-05-05 15:37:29 +0900

    finished c.23

--- c/lox/debug.c
+++ c/lox/debug.c
@@ -30,6 +30,13 @@ static int byteInstruction(const char* name, Chunk* chunk, int offset){
     return offset + 2;
 }
 
+static int jumpInstruction(const char* name, int sign, Chunk* chunk, int offset){
+    uint16_t jump = (uint16_t)(chunk->code[offset + 1] << 8);
+    jump |= chunk->code[offset + 2];
+    printf("%-16s %4d -> %d\n", name, offset, offset + 3 + sign * jump);
+    return offset + 3;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset){
     printf("%04d", offset);
     if(offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]){
@@ -40,7 +47,7 @@ int disassembleInstruction(Chunk* chunk, int offset){
     uint8_t instruction = chunk->code[offset];
     switch(instruction){
         case OP_CONSTANT:
-        return constantInstruction("OP_CONSTANT", chunk, offset);
+            return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_NIL:
             return simpleInstruction("OP_NIL", offset);
         case OP_TRUE:
@@ -66,21 +73,27 @@ int disassembleInstruction(Chunk* chunk, int offset){
         case OP_LESS:
             return simpleInstruction("OP_LESS", offset);
         case OP_ADD:
-        return simpleInstruction("OP_ADD", offset);
+            return simpleInstruction("OP_ADD", offset);
         case OP_SUBTRACT:
-        return simpleInstruction("OP_SUBTRACT", offset);
+            return simpleInstruction("OP_SUBTRACT", offset);
         case OP_MULTIPLY:
-        return simpleInstruction("OP_MULTIPLY", offset);
+            return simpleInstruction("OP_MULTIPLY", offset);
         case OP_DIVIDE:
-        return simpleInstruction("OP_DIVIDE", offset);
+            return simpleInstruction("OP_DIVIDE", offset);
         case OP_NOT:
-        return simpleInstruction("OP_NOT", offset);
+            return simpleInstruction("OP_NOT", offset);
         case OP_NEGATE:
-        return simpleInstruction("OP_NEGATE", offset);
+            return simpleInstruction("OP_NEGATE", offset);
         case OP_PRINT:
-        return simpleInstruction("OP_PRINT", offset);
+            return simpleInstruction("OP_PRINT", offset);
+        case OP_JUMP:
+            return jumpInstruction("OP_JUMP", 1, chunk, offset);
+        case OP_JUMP_IF_FALSE:
+            return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
+        case OP_LOOP:
+            return jumpInstruction("OP_LOOP", -1, chunk, offset);
         case OP_RETURN:
-        return simpleInstruction("OP_RETURN", offset);
+            return simpleInstruction("OP_RETURN", offset);
         default:
         printf("Unknow opcode %d\n", instruction);
         return offset + 1;
